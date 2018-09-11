c789e25ad1d71ad8ec32023bdc92f40dd715ae0a
Elena Vieru <33833374+elenavieru@users.noreply.github.com>
2018-09-11 11:48:17 +0200

    Update style.js

--- style.js
+++ style.js
@@ -7,39 +7,64 @@
 		page = blink.currentPage;
 
 	heyfriends_demoStyle.prototype = {
+		//BK-15873 añadimos el estilo basic como parent para la herencia de los estilos del CKEditor
+		parent: blink.theme.styles.basic.prototype,
 		bodyClassName: 'content_type_clase_heyfriends_demo',
 		ckEditorStyles: {
 			name: 'heyfriends_demo',
 			styles: [
 
-				{ name: 'Título 1', element: 'h4', attributes: { 'class': 'bck-title bck-title1'} },
-				{ name: 'Título 2', element: 'h4', attributes: { 'class': 'bck-title bck-title2'} },
+				{ name: 'Título 1', element: 'h4', attributes: { 'class': 'bck-title1'} },
 
-				{ name: 'Lista ordenada1', element: 'ol', attributes: { 'class': 'bck-ol bck-ol1' } },
-				{ name: 'Lista ordenada2', element: 'ol', attributes: { 'class': 'bck-ol bck-ol2' } },
-				{ name: 'Lista ordenada3', element: 'ol', attributes: { 'class': 'bck-ol bck-ol3' } },
-				{ name: 'Lista ordenada4', element: 'ol', attributes: { 'class': 'bck-ol bck-ol4' } },
+				{ name: 'Énfasis', element: 'span', attributes: { 'class': 'bck-enfasis' }},
+				{ name: 'Énfasis2', element: 'span', attributes: { 'class': 'bck-enfasis2' }},
 
-				{ name: 'Caja 1', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box bck-box1' } },
-				{ name: 'Caja 2', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box bck-box2' } },
-				{ name: 'Caja 3', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box bck-box3' } },
+				{ name: 'Ejemplo', element: 'span', attributes: { 'class': 'bck-example' }},
 
-				{ name: 'Icono Draw', element: 'span', attributes: { 'class': 'icon icon-draw' } },
-				{ name: 'Icono Write', element: 'span', attributes: { 'class': 'icon icon-write' } },
-				{ name: 'Icono Speaking', element: 'span', attributes: { 'class': 'icon icon-speaking' } },
-				{ name: 'Icono Reading', element: 'span', attributes: { 'class': 'icon icon-reading' } },
-				{ name: 'Icono Star', element: 'span', attributes: { 'class': 'icon icon-star' } }
+				{ name: 'Lista ordenada1', element: 'ol', attributes: { 'class': 'bck-ol1' } },
+				{ name: 'Lista ordenada2', element: 'ol', attributes: { 'class': 'bck-ol2' } },
+
+				{ name: 'Lista desordenada', element: 'ul', attributes: { 'class': 'bck-ul1' } },
+				{ name: 'Lista desordenada2', element: 'ul', attributes: { 'class': 'bck-ul2' } },
+
+				{ name: 'Tabla sin bordes', element: 'table', attributes: { 'class': 'bck-table bck-table-borderless' } },
+				{ name: 'Tabla normal', element: 'table', attributes: { 'class': 'bck-table bck-table-normal' } },
+				{ name: 'Tabla borde azul', element: 'table', attributes: { 'class': 'bck-table bck-table-bordered' } },
+				{ name: 'Celda azul', element: 'td', attributes: { 'class': 'bck-td' } },
+
+				{ name: 'Caja1', type: 'widget', widget: 'blink_box', attributes: { 'class': 'box-1' } },
+				{ name: 'Caja2', type: 'widget', widget: 'blink_box', attributes: { 'class': 'box-2' } },
+				{ name: 'Título Caja 2', element: 'span', attributes: { 'class': 'box-title-secondary' } },
+				{ name: 'Caja3', type: 'widget', widget: 'blink_box', attributes: { 'class': 'box-3' } },
+				{ name: 'Caja4', type: 'widget', widget: 'blink_box', attributes: { 'class': 'box-4' } },
+				{ name: 'Caja5', type: 'widget', widget: 'blink_box', attributes: { 'class': 'box-5' } },
+				{ name: 'Caja6', type: 'widget', widget: 'blink_box', attributes: { 'class': 'box-6' } },
+				{ name: 'Caja7', type: 'widget', widget: 'blink_box', attributes: { 'class': 'box-7' } },
+				{ name: 'Caja8', type: 'widget', widget: 'blink_box', attributes: { 'class': 'box-8' } },
+
+				{ name: 'Icono Lápiz', element: 'span', attributes: { 'class': 'icon icon-lapiz' } },
+				{ name: 'Icono Lápiz1', element: 'span', attributes: { 'class': 'icon icon-lapiz1' } },
+				{ name: 'Icono Lápiz2', element: 'span', attributes: { 'class': 'icon icon-lapiz2' } },
+				{ name: 'Icono Lápiz3', element: 'span', attributes: { 'class': 'icon icon-lapiz3' } },
+				{ name: 'Icono Conversación', element: 'span', attributes: { 'class': 'icon icon-conversation' } },
+				{ name: 'Icono Conversación1', element: 'span', attributes: { 'class': 'icon icon-conversation1' } },
+				{ name: 'Icono Conversación2', element: 'span', attributes: { 'class': 'icon icon-conversation2' } },
+				{ name: 'Icono Conversación3', element: 'span', attributes: { 'class': 'icon icon-conversation3' } },
+				{ name: 'Icono Cloud', element: 'span', attributes: { 'class': 'icon icon-cloud' } },
+				{ name: 'Icono Cloud1', element: 'span', attributes: { 'class': 'icon icon-cloud1' } },
+				{ name: 'Icono Cloud2', element: 'span', attributes: { 'class': 'icon icon-cloud2' } },
+				{ name: 'Icono Cloud3', element: 'span', attributes: { 'class': 'icon icon-cloud3' } }
 			]
 		},
 
 		init: function () {
-			var parent = blink.theme.styles.basic.prototype;
-			parent.init.call(this);
+			//BK-15873 Utilizamos this.parent declarada al inicio de la clase
+			this.parent.init.call(this);
 			this.addActivityTitle();
 			if(window.esWeb) return;
 			this.removeFinalSlide();
+			this.addSlideNavigators();
 			this.handleScrollEnd();
-			this.setTooltip();
 			window.editar && this.configEditor();
 
 			if ($('.navbar-bottom').length > 0) {
@@ -58,6 +83,11 @@
 
 		},
 
+		removeFinalSlide: function () {
+			//BK-15873 Utilizamos this.parent declarada al inicio de la clase
+			this.parent.removeFinalSlide.call(this, true);
+		},
+
 		configEditor: function (editor) {
 			editor.dtd.$removeEmpty['span'] = false;
 		},
@@ -66,7 +96,10 @@
 		addActivityTitle: function () {
 			if (!blink.courseInfo || !blink.courseInfo.unit) return;
 			$('.libro-left').find('.title').html(function () {
-				return $(this).html().trim() + ' > ' + blink.courseInfo.unit;
+				var initials =  blink.activity.name.match(/\b(\w)/g);
+
+				initials = initials.slice(0, 2);
+				return $(this).html().trim() + ' - ' + initials.join('') + ' > ' + blink.courseInfo.unit;
 			});
 		},
 
@@ -84,11 +117,7 @@
 
  		},
 
-		setTooltip: function () {},
-
-		getEditorStyles: function () {
-			return this.ckEditorStyles;
-		}
+		//BK-15873 Quitamos la funcion getEditorStyles para que la herede de basic
 	};
 
 	heyfriends_demoStyle.prototype = _.extend({}, new blink.theme.styles.basic(), heyfriends_demoStyle.prototype);
@@ -96,3 +125,51 @@
 	blink.theme.styles.heyfriends_demo = heyfriends_demoStyle;
 
 })( blink );
+
+$(document).ready(function () {
+
+    $('.box-2').each(function(){
+    	$(this).prepend('<div class="box-title">' +
+    		'<span class="box-title-main">PROJECT</span>' +
+    		'<span class="box-pico"></span>' +
+    		'<span class="box-title-secondary">Preview</span>' +
+    	'</div>' +
+    	'<div class="box-back-title">' +
+    		'<span class="box-title-main">PROJECT</span>' +
+    		'<span class="box-pico"></span>' +
+    		'<span class="box-title-secondary">Preview</span>' +
+    	'</div>');
+    });
+
+    $('.box-3, .box-4, .box-5, .box-8').each(function(){
+    	$(this).prepend('<div class="box-title">' +
+    		'<span class="box-title-main">What about <strong>you</strong>?</span>' +
+    		'<span class="box-pico"></span>' +
+    	'</div>').promise().done(function(){
+    		if( $(this).hasClass('box-4') ) {
+    			$(this).find('.box-title-main').html('Writing rule');
+    		} else if ($(this).hasClass('box-5')) {
+    			$(this).find('.box-title-main').html('I can...');
+    		} else if ($(this).hasClass('box-8')) {
+    			$(this).find('.box-title-main').html('Pronunciation');
+    		}
+    	});
+    });
+
+    $('.box-6, .box-7').each(function(){
+    	$(this).prepend('<div class="box-title">!</div>')
+    		   .promise().done(function(){
+		    		if( $(this).hasClass('box-7') ) {
+		    			$(this).find('.box-title').html('>');
+		    		}
+		    	});
+    });
+
+    $('.ultima h3').each(function(){
+    	$(this).prepend('<div class="shadow-title">' +
+    		'END OF PROJECT' +
+    		'<span class="shadow-pico"></span>' +
+    	'</div>');
+    });
+
+});
